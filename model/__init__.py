c84c885a84f1e5df9175cba191abc4e87fe09535
Niklas Holtz <nholtz1995@gmail.com>
2021-06-09 12:35:22 +0200

    Added new package

--- model/__init__.py
+++ model/__init__.py
@@ -4,4 +4,5 @@ from .Encoder import EncoderLayer
 from .MultiHeadAttention import MultiHeadAttention
 from .Transformer import Transformer
 from .Util import (scaled_dot_product_attention, positional_encoding, point_wise_feed_forward_network)
-import tokenizers
+from .tokenizers.SmilesTokenizer import SmilesTokenizer
+from .tokenizers.SelfiesTokenizer import SelfiesTokenizer
