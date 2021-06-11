0d91e99ca8cadd1e9c9af072f1c36b27865b070e
Niklas Holtz <nholtz1995@gmail.com>
2021-06-11 13:04:49 +0200

    Added init methods

--- model/translators/ForwardSearch.py
+++ model/translators/ForwardSearch.py
@@ -4,7 +4,7 @@ import numpy as np
 import tensorflow as tf
 from rdkit import Chem
 
-from .. import BeamSearchTranslator
+from . import BeamSearchTranslator
 from ..Transformer import Transformer
 
 
